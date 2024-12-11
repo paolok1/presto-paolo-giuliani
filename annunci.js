cba2e928bcdb21db124a514b19fa44ae14132ace
paolok1 <gpaolok1@gmail.com>
2024-12-11 14:12:15 +0100

    PROGETTO PRESTO 2

---
+++ annunci.js
@@ -0,0 +1,143 @@
+// .json: javascript object notification.
+
+// API: sono delle chiavi che ci permettono di raggiungere un .json online.
+
+// .fetch():è una chiamata asincrona che ci permette
+// di collegare il nostro foglio JS ad un link esterno(JSON).
+// ci restituisce i dati prelevati sotto forma di PROMISE(PROMESSA) ossia
+// promette che ci restituirà questi dati.
+// .then(): questo metodo ci permette di convertire la PROMISE nel dato strutturale e di
+//  poterlo utilizzare come tale su javascript.
+
+// 1. Fetch(): collego al json e ottengo una Promise;
+// 2. .Then(): converto la Promise in un dato strutturale JS;
+// 3. .Then(): utilizzare il dato ottenuto.
+
+// .json(): metodo della promise che mi permette di convertirla in OGGETTO JS.
+
+fetch('./annunci.json').then((response) => response.json()).then( (data)=>{
+
+    data.sort((a,b)=> a.price - b.price);
+
+let radioWrapper = document.querySelector('#radioWrapper');
+let cardWrapper = document.querySelector('#cardWrapper');
+
+
+function radioCreate () {
+    let categories = data.map( (annuncio) => annuncio.category );
+
+//     let uniqueCategories = [];
+
+//     categories.forEach( (category) => {
+//             if (!uniqueCategories.includes(category )) {
+//                 uniqueCategories.push(category);
+//             }
+//     });
+
+// Set: Classe che mi restituisce, partendo da un Array, un nuovo oggetto di tipo Set il quale 
+// contiene solo valori univoci.
+// Array.from() : mi permette di convertire un Array-like in un Array.
+// esempio con Set : let uniqueCategories = new Set (categories);
+// esempio con Array.from:
+let uniqueCategories = Array.from (new Set(categories));
+uniqueCategories.forEach((category)=>{
+    let div = document.createElement('div');
+    div.classList.add('form-check');
+    div.innerHTML = `
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="${category}">
+                <label class="form-check-label" for="${category}">
+                    ${category}
+                </label>
+    `;
+    radioWrapper.appendChild(div);
+})
+console.log(uniqueCategories);
+
+}   
+
+radioCreate();
+
+
+function truncateWord(string) {
+   if (string.length > 15) {
+        return string.split(' ')[0] + ('...');
+   }else{
+    return string;
+   }
+}
+
+
+
+function showCards(array) {
+    cardWrapper.innerHTML = '';
+    array.forEach( (annuncio, i)=>{
+        let div = document.createElement('div');
+        div.classList.add('card-custom');
+        div.innerHTML = `
+            <img src ="https://picsum.photos/${300 + i}" alt = "immagine casuale" class ="img-fluid img-card"> 
+            <p class="h2" title = "${annuncio.name}">${truncateWord(annuncio.name)}</p>
+            <p class="h4">${annuncio.category}</p>
+            <p class="lead">${annuncio.price} €</p>
+        `;
+        cardWrapper.appendChild(div);
+    })
+}
+
+showCards(data);
+
+
+function filterCategories(categoria){
+    // in questa funzione ho bisogno di ottenere un nuovo Array partendo da data e gli elementi
+    // del nuovo Array dovranno soddisfare la condizione per la quale la loro category sia uguale 
+    // alla categoria che stiamo passando alla funzione.
+    if (categoria != 'all') {
+        
+        let filtered = data.filter((annuncio)=> annuncio.category == categoria )
+        showCards(filtered);
+    }else{
+        showCards(data);
+    }
+}
+    let radioButtons = document.querySelectorAll('.form-check-input');
+        radioButtons.forEach((button) =>{
+            button.addEventListener('click', ()=>{
+                filterCategories(button.id);
+            })
+        })
+
+        let priceInput = document.querySelector('#priceInput');
+        let priceValue = document.querySelector('#priceValue');
+
+    function setPriceInput() {
+        // dopo aver catturato l'INPUT (tag INPUT) voglio settare come proprietà MAX dello stesso
+        // il valore più alto tra i price di ogni prodotto. Per farlo avrò bisogno, quindi, di un Array 
+        // che contenga solo i prezzi, a quel punto lo ordino in maniera crescente o decrescente e 
+        //  prendermi l'elemento con il valore più alto.
+
+        let prices = data.map((annuncio)=> +annuncio.price);
+        prices.sort((a, b)=> a - b );
+        let maxPrice = Math.ceil(prices.pop());
+        priceInput.max = maxPrice;
+        priceInput.value = maxPrice;
+        priceValue.innerHTML = maxPrice;
+
+        
+        
+    }
+
+        setPriceInput();
+
+
+    function filterByPrice() {
+        let filtered = data.filter((annuncio)=> annuncio.price <= priceInput.value);
+        showCards(filtered)
+    }
+    
+        priceInput.addEventListener('input', ()=>{
+            priceValue.innerHTML = priceInput.value;
+            filterByPrice();
+        })
+    
+} );   
+
+
--- main.js
+++ main.js
@@ -1,28 +1,164 @@
 let navbar = document.querySelector('#navbar');
 let links = document.querySelectorAll('.nav-link');
 let logoNavbar = document.querySelector('#logoNavbar');
-
-console.dir(logoNavbar);
+let spadaLaser = document.querySelector('#spadaLaser');
+let collapse = document.querySelector('#collapse');
+let check = false;
+console.dir(spadaLaser);
 
 window.addEventListener('scroll',()=>{
     let scrolled = window.scrollY;
     if (scrolled > 0) {
         navbar.classList.remove('bg-black');
         navbar.classList.add('bg-yellow');
+        collapse.classList.remove('bg-black');
+        collapse.classList.add('bg-yellow');
         navbar.style.height = '70px';
         links.forEach((link)=>{
             link.style.color = 'var(--black)';
         });
-        logoNavbar.src = "http://127.0.0.1:5500/media/logo-r.png"
+        logoNavbar.src = "http://127.0.0.1:5500/media/logo-b.png"
+        spadaLaser.src = "http://127.0.0.1:5500/media/spadablack.png"
     }else{
         navbar.classList.add('bg-black');
         navbar.classList.add('bg-yellow');
+        collapse.classList.add('bg-black');
+        collapse.classList.add('bg-yellow');
         navbar.style.height = '140px';
         links.forEach((link)=>{
             link.style.color = 'var(--yellow)';
         }) ;
-        logoNavbar.src = "http://127.0.0.1:5500/media/logo-y.png" 
+        logoNavbar.src = "http://127.0.0.1:5500/media/logo-y.png"
+        spadaLaser.src = "http://127.0.0.1:5500/media/spadayellow.png"
     }
 });
 
+   spadaLaser.addEventListener('click', ()=>{
+    if (check == false) {
+    spadaLaser.style.transform = 'rotate(-180deg)';
+    check = true;
+    }else{
+        spadaLaser.style.transform = 'rotate(0deg)';
+        check = false;   
+    }
+     
+   })
+
+
+    // Chiamate Asincrone
+
+    // setInterval(): crea un loop infinito in cui possiamo gestire la durata delle singole iterazioni.
+    // il setInterval è una funzione che vuole 2 parametri e viene inserito in una variabile. il primo 
+    // parametro è la CALLBACK()=>
+    // il secondo parametro è l'intervallo di tempo che deve passare tra un iterazione e l'altra.
+    // clearInterval(): pulisce l'intervallo e interrompe il loop.
+    // setTimeout(): fa partire un blocco di istruzioni dopo tot secondi. 
+
+
+    let firstNumber = document.querySelector('#firstNumber');
+    let secondNumber = document.querySelector('#secondNumber');
+    let thirdNumber = document.querySelector('#thirdNumber');    
+        
+    function createInterval(n, element, time){
+            let counter = 0;
+
+            let interval = setInterval( ()=>{
+            if(counter < n){
+                counter++
+                element.innerHTML = counter;
+            }else{
+               
+            clearInterval(interval);
+            }
     
+        }, time);
+
+        setTimeout( () => {
+            confirm = true;
+        }, 8000);
+      
+    }
+
+
+
+    // intersectionObserver: è una Classe del BROWSER (non del linguaggio) che si occupa di far scattare
+    // una funzione nel momento in cui nel browser gli elementi HTML che gli indichiamo sono visibili.
+    // new: è una Keyword che mi PERMETTE DI GENERARE UN OGGETTO partendo da una Classe.
+    // nella variabile (observer) stiamo creando un OGGETTO di Classe intersectionObserver. 
+    // in questo OGGETTO scatta una CALLBACK la quale accetta un qualsiasi numero di parametri salvandoli nel 
+    // parametro formale ENTRIES (che è un Array ( entries = [] ), quindi si utilizzerà il forEach).
+    let observer = new IntersectionObserver((entries)=>{
+        entries.forEach( (entry)=> {
+
+            if(entry.isIntersecting && confirm){
+            createInterval(100, firstNumber, 100);
+            createInterval(200, secondNumber, 50);
+            createInterval(300, thirdNumber, 20);
+            confirm = false;
+            }
+        });
+    } );
+    
+    observer.observe(firstNumber);
+
+
+let reviews = [
+    {user : 'Paolo', description : `Il più bel sito del mondo`, rank : 5},
+    {user : 'Claudio', description : `Il peggio in assoluto`, rank : 1},
+    {user : 'Marco', description : `Non male ma si può fare meglio`, rank : 3},
+    {user : 'Giulio', description : `Top senza eguali`, rank : 5}
+
+]
+
+let swiperWrapper = document.querySelector('.swiper-wrapper');
+
+reviews.forEach((recensione)=>{
+    let div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+                <div class="card-review">
+                    <p class="lead text-center">${recensione.description}</p>
+                    <p class="h4 text-center">${recensione.user}</p>
+                    <div class="d-flex justify-content-center star">
+    
+                    </div>
+                </div>`
+                swiperWrapper.appendChild(div);
+
+});
+
+let stars = document.querySelectorAll('.star');
+stars.forEach((star, index)=>{
+    for(let i = 1; i<= reviews[index].rank; i++ ){
+        let icon = document.createElement('i');
+        icon.classList.add('fa-solid', 'fa-star');
+        star.appendChild(icon);
+    }
+
+    let difference = 5 - reviews[index].rank;
+    for(let i = 1; i<= difference; i++ ){
+        let icon = document.createElement('i');
+        icon.classList.add('fa-regular', 'fa-star');
+        star.appendChild(icon);
+    }
+})
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        effect: 'flip',
+        grabCursor : true,
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+      
+      });
+
